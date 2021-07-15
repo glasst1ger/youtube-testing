9f0af81c705323674ec9d428d7f7f8e0c2541b4c
glasst1ger <szak.viktor@gmail.com>
2021-07-15 15:23:33 +0200

    login half done

--- src/test/java/YoutubeTesting.java
+++ src/test/java/YoutubeTesting.java
@@ -2,14 +2,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class YoutubeTesting {
 
     private WebDriver webDriver;
-    private By usernameField = By.id("");
-    private By passwordField = By.id("");
+    private By loginField = By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div/div/div[1]/div[1]/div/div/a");
+    private By usernameField = By.id("identifierId");
+    private By passwordField = By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input");
+
+    private WebDriverWait getWebDriverWait() {
+        return new WebDriverWait(webDriver, 10);
+    }
 
 
     @BeforeAll
@@ -25,7 +34,22 @@ public class YoutubeTesting {
 
     @Test
     public void testLoginWiki() {
-        webDriver.get("https://wikipedia.org");
+        webDriver.get("https://youtube.com");
+        webDriver.manage().window().maximize();
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable((loginField))).click();
+        webDriver.findElement(usernameField).sendKeys("autiteszti");
+        WebElement textbox = webDriver.findElement(usernameField);
+        textbox.sendKeys(Keys.ENTER);
+
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable((passwordField))).click();
+        webDriver.findElement(passwordField).sendKeys("Codecool123");
+        WebElement textbox2 = webDriver.findElement(passwordField);
+        textbox2.sendKeys(Keys.ENTER);
+
+
+
     }
+
+    // //*[@id="identifierId"]
 
 }

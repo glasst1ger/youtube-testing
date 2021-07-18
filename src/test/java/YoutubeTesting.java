8ba32b364f51194dacbe816e7e96ed6610c0c865
rausch.nita <rausch.nita@gmail.com>
2021-07-18 11:58:28 +0200

    Setting Button test is passed

--- src/test/java/YoutubeTesting.java
+++ src/test/java/YoutubeTesting.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class YoutubeTesting {
 
@@ -24,6 +27,8 @@ public class YoutubeTesting {
     private By stopButton = By.id("movie_player");
     private By wideScreenButton = new By.ByCssSelector(".ytp-size-button.ytp-button");
     private By fullScreenButton = new By.ByCssSelector(".ytp-fullscreen-button.ytp-button");
+    private By likeButton = By.id("top-level-buttons-computed");
+    private By settingButton = new By.ByCssSelector(".ytp-button.ytp-settings-button");
 
     @BeforeAll
     static void setDriverProperty() {
@@ -32,8 +37,11 @@ public class YoutubeTesting {
 
     @BeforeEach
     public void setWebDriver() {
-        webDriver = new ChromeDriver();
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--disable-notifications");
+        webDriver = new ChromeDriver(ops);
     }
+
 
     @Test
     public void testLoginYouTube() {
@@ -47,18 +55,17 @@ public class YoutubeTesting {
         getWebDriverWait().until(ExpectedConditions.elementToBeClickable((passwordField))).click();
         webDriver.findElement(passwordField).sendKeys("Codecool123");
         webDriver.findElement(passwordField).sendKeys(Keys.ENTER);
-
-
     }
     @Test
     public void testStopPlayButton() {
         testLoginYouTube();
         getWebDriverWait().until(ExpectedConditions.elementToBeClickable((searchField))).click();
-
         webDriver.get("https://www.youtube.com/watch?v=Nd31XiSGJLw");
 
-        for (int i = 0; i < 10; i++) {
-            webDriver.findElement(stopButton).click();
+        for (int i = 0; i < 9; i++) {
+           // getWebDriverWait().until(ExpectedConditions.elementToBeClickable((stopButton))).click();
+            webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
             webDriver.findElement(stopButton).click();
         }
 
@@ -67,14 +74,41 @@ public class YoutubeTesting {
     public void testSizeButton() {
         testLoginYouTube();
         getWebDriverWait().until(ExpectedConditions.elementToBeClickable((searchField))).click();
-
         webDriver.get("https://www.youtube.com/watch?v=Nd31XiSGJLw");
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
+            webDriver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
             webDriver.findElement(wideScreenButton).click();
-
         }
         webDriver.findElement(fullScreenButton).click();
     }
+    @Test
+    public void testLikeButton() {
+        testLoginYouTube();
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable((searchField))).click();
+        webDriver.get("https://www.youtube.com/watch?v=Nd31XiSGJLw");
 
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable((likeButton))).click();
+
+        webDriver.findElement(likeButton).click();
+
+    }
+    @Test
+    public void testVideoSettingButton() {
+        testLoginYouTube();
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable((searchField))).click();
+        webDriver.get("https://www.youtube.com/watch?v=Nd31XiSGJLw");
+
+        webDriver.findElement(settingButton).click();
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='Kommentárok']"))).click();
+
+        webDriver.findElement(settingButton).click();
+        webDriver.findElement(settingButton).click();
+
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='Lejátszási sebesség']"))).click();
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='2']"))).click();
+
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='Minőség']"))).click();
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='144p']"))).click();
+    }
 }
